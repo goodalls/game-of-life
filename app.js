0bf70452e814acd902342e0beb6830126aca886f
Jeff Goodall <goodalls@live.com>
2023-01-13 16:32:14 -0700

    added checkSurrounding function

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
+//x is horizontal y is vertical
+
 document.querySelector("canvas").height = innerHeight;
 document.querySelector("canvas").width = innerWidth;
 const c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
-// const objectTest = [
-//   (x - 5, y),
-//   (x + 5, y),
-//   (x, y - 5),
-//   (x, y + 5),
-//   (x - 5, y + 5),
-//   (x - 5, y - 5),
-//   (x + 5, y + 5),
-//   (x + 5, y - 5),
-// ];
 let objectArray = [];
-let objectTest = [];
+let testArray = [];
+
+function Cell(x, y, alive) {
+  this.x = x;
+  this.y = y;
+  this.alive = alive === 1 ? true : false;
+  this.neighbor = 0;
+}
 
 function createBoard() {
-  let height = c.height;
   let width = c.width;
+  let height = c.height;
   let x = 0;
   let y = 0;
-  let cells = Math.floor((height * width) / 5); // each cell is 5px
-  console.log(height, width);
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       let alive = Math.floor(Math.random() * 2);
-      objectArray.push(new Cell(x, y, 0, alive));
+      objectArray.push(new Cell(x, y, alive));
       square(x, y, alive);
       y += 5;
-      console.log(alive);
     }
     x += 5;
   }
 }
-createBoard();
+
+if (objectArray.length <= 1) {
+  createBoard();
+}
 
 function square(x, y, alive) {
   ctx.fillRect(x, y, 5, 5);
@@ -41,19 +40,38 @@ function square(x, y, alive) {
   ctx.stroke();
 }
 
-function test() {
-  for (let x = 0; x < objectArray.length; x++) {
-    const object = objectArray[x];
-    for (let i = 0; i < objectTest.length; i++) {
-      const objectTest = object[i];
-      //test the 8 objects surrounding
-    }
-  }
-}
+// function test(x, y) {
+//   const objectTest = [
+//     (x - 5, y),
+//     (x + 5, y),
+//     (x, y - 5),
+//     (x, y + 5),
+//     (x - 5, y + 5),
+//     (x - 5, y - 5),
+//     (x + 5, y + 5),
+//     (x + 5, y - 5),
+//   ];
+// };
 
-function Cell(x, y, neibors, alive) {
-  this.x = x;
-  this.y = y;
-  this.neibors = neibors;
-  this.alive = alive;
+function checkSurrounding(object1) {
+  let numAlive = 0;
+  objectArray.forEach((object2) => {
+    if (
+      object2.x < object1.x + 5 &&
+      object2.x > object1.x - 5 ||
+      object2.y < object1.y + 5 &&
+      object2.y > object1.y - 5 &&
+      object1 != object2
+    ) {
+      if (object2.alive === true) {
+        numAlive++;
+      }
+    }
+  });
+  console.log(numAlive);
+  if (numAlive === 2 || numAlive === 3) {
+    object1.alive = true;
+  } else {
+    object1.alive = false;
+  }
 }
