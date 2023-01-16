3af56bb6967803687d084b2b7a26b17a807178b7
Jeff Goodall <goodalls@live.com>
2023-01-15 18:34:58 -0700

    added requestAnimationFrame

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 //x is horizontal y is vertical
 
-let height = (document.querySelector("canvas").height = innerHeight);
-let width = (document.querySelector("canvas").width = innerWidth);
+// let height = (document.querySelector("canvas").height = innerHeight);
+// let width = (document.querySelector("canvas").width = innerWidth);
+let height = 500;
+let width = 500;
+
 const c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
 let objectArray = [];
-let testArray = [];
 
 function Cell(x, y, alive) {
   this.x = x;
   this.y = y;
   this.alive = alive === 1 ? true : false;
-  this.nextAlive = NaN;
+  this.nextAlive = false;
 }
 
 function createBoard() {
@@ -36,7 +38,7 @@ if (objectArray.length <= 1) {
 
 function square(x, y, alive) {
   ctx.fillRect(x, y, 5, 5);
-  ctx.fillStyle = alive === 1 ? "black" : "white";
+  ctx.fillStyle = alive ? "black" : "white";
   ctx.stroke();
 }
 
@@ -50,7 +52,6 @@ function checkSurrounding(object1) {
         numAlive++;
       }
     }
-
     //upper middle cell
     if (object1.x === object2.x && object1.y - 5 === object2.y) {
       if (object2.alive === true) {
@@ -100,15 +101,11 @@ function checkSurrounding(object1) {
       }
     }
   });
-  console.log(numAlive);
   if (numAlive === 2 || numAlive === 3) {
-    object1.nextAlive = true;
-  } else if (numAlive === 4) {
-    object1.nextAlive = true;
-  } else {
-    object1.nextAlive = false;
+    object1.alive = true;
+  }  else {
+    object1.alive = false;
   }
-  return object1;
 }
 
 function recreateBoard() {
@@ -117,31 +114,30 @@ function recreateBoard() {
   });
 }
 function gameLoop() {
-  //clear Canvas
-  ctx.clearRect(0, 0, width, height);
-
-  //loop through objectArray to test for Neibors
+  //loop through objectArray to test for Neibours
   objectArray.forEach((object) => {
     checkSurrounding(object);
   });
 
   // update objectArray
-  let count = 0;
-  for (let i = 0; i < objectArray.length; i++) {
-    this.objectArray[i].alive = this.objectArray[i].nextAlive;
-    if (this.objectArray[i].nextAlive) {
-      count++;
-    }
-  }
+  // let count = 0;
+  // for (let i = 0; i < objectArray.length; i++) {
+  //   objectArray[i].alive = objectArray[i].nextAlive;
+  //   if (objectArray[i].nextAlive) {
+  //     count++;
+  //   }
+  // }
+
+  //clear Canvas
+  ctx.clearRect(0, 0, width, height);
+
   //and redraw
   recreateBoard();
 
   //recall gameLoop function with setTimeout + Conditional 50% DEAD CELLS
   setTimeout(() => {
-    if (count <= objectArray.length * 0.5) {
-      gameLoop();
-    }
-  }, 1000);
+    window.requestAnimationFrame(gameLoop);
+}, 50);
+console.log('tick');
 }
-console.log(objectArray);
-document.querySelector("canvas").addEventListener("click", gameLoop);
+document.querySelector("canvas").addEventListener("click", ()=>{window.requestAnimationFrame(gameLoop)});
